78f9a916e340d2d6f8092eaafa9166d1b9a9b9c8
Michael Rodrigues <mrodrig@users.noreply.github.com>
2022-08-19 20:24:54 +0000

    tweaked button sizes

--- Makefile
+++ Makefile
@@ -2,6 +2,5 @@
 	mkdir -p build
 
 windows: .setup-builddir main.go
-	# fyne package -os windows -icon myapp.png
 	GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc fyne package -os windows
 	mv *.exe build/
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os/exec"
 
+	"fyne.io/fyne"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
 )
@@ -24,27 +25,35 @@ func main() {
 	app := app.New()
 	window := app.NewWindow("GoPowerManager")
 
+	// Create buttons
+	shutdownButton := widget.NewButton("Shutdown", func() {
+		var flags = []string{"/s", "/f"}
+		runCommand(flags)
+	})
+	rebootButton := widget.NewButton("Reboot", func() {
+		var flags = []string{"/r", "/f"}
+		runCommand(flags)
+	})
+	hibernateButton := widget.NewButton("Hibernate", func() {
+		var flags = []string{"/h"}
+		runCommand(flags)
+	})
+	quitButton := widget.NewButton("Quit", func() {
+		app.Quit()
+	})
+
+	// Resize them all to a consistent size
+	shutdownButton.Resize(fyne.NewSize(250, 80))
+	rebootButton.Resize(fyne.NewSize(250, 80))
+	hibernateButton.Resize(fyne.NewSize(250, 80))
+	quitButton.Resize(fyne.NewSize(250, 80))
+
+	// Setup the GUI layout
 	window.SetContent(widget.NewVBox(
-		widget.NewHBox(
-			widget.NewButton("Shutdown", func() {
-				var flags = []string{"/s", "/f"}
-				runCommand(flags)
-			}),
-			widget.NewButton("Reboot", func() {
-				var flags = []string{"/r", "/f"}
-				runCommand(flags)
-			}),
-		),
-		widget.NewHBox(
-			widget.NewButton("Hibernate", func() {
-				var flags = []string{"/h"}
-				runCommand(flags)
-			}),
-			widget.NewButton("Quit", func() {
-				app.Quit()
-			}),
-		),
+		widget.NewHBox(shutdownButton, rebootButton),
+		widget.NewHBox(hibernateButton, quitButton),
 	))
 
+	// Show the interface
 	window.ShowAndRun()
 }
